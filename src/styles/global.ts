bc3ec2f66ab1b62757b23f887ef3a0eb792e879b
amalcaraz <amalcaraz89@gmail.com>
2023-02-18 18:58:25 +0100

    added margin and padding sizes classes

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -19,6 +19,12 @@ export const GlobalStyle = createGlobalStyle`
         color: ${theme.color.text};
       }
 
+      p {
+        color: ${theme.color.text}b3;
+      }
+
+      ${sizingClasses(theme)}
+
       ${colorClasses(theme)}
       ${typoClasses(theme)}
       ${effectClasses(theme)}
@@ -26,25 +32,41 @@ export const GlobalStyle = createGlobalStyle`
   }}
 `
 
+function sizingClasses(theme: DefaultTheme) {
+  return css`
+    .m-0 { margin: 0; }
+    .mt-0, .my-0 { margin-top: 0; }
+    .mb-0, .my-0 { margin-bottom: 0; }
+    .ml-0, .mx-0 { margin-left: 0; }
+    .mr-0, .mx-0 { margin-right: 0; }
+
+    .p-0 { padding: 0; }
+    .pt-0, .py-0 { padding-top: 0; }
+    .pb-0, .py-0 { padding-bottom: 0; }
+    .pl-0, .px-0 { padding-left: 0; }
+    .pr-0, .px-0 { padding-right: 0; }
+  `
+}
+
 function colorClasses(theme: DefaultTheme) {
   return css`
     /* COLORS */
     ${Object.keys(theme.color).map(
-      (color) => css`
+    (color) => css`
         .bg-${color} {
           ${getBackgroundColorCss(color)}
         }
       `,
-    )}
+  )}
 
     /* GRADIENTS */
     ${Object.keys(theme.gradient).map(
-      (color) => css`
+    (color) => css`
         .gr-${color} {
           ${getBackgroundGradientCss(color)}
         }
       `,
-    )}
+  )}
   `
 }
 
@@ -52,7 +74,7 @@ function typoClasses(theme: DefaultTheme) {
   return css`
     /* TYPOS */
     ${Object.entries(theme.typo).map(
-      ([k, v]) => css`
+    ([k, v]) => css`
         .tp-${k} ${v.tag ? `, ${k}` : ''} {
           ${getTypoCss(k)}
           margin-top: 20px;
@@ -60,16 +82,16 @@ function typoClasses(theme: DefaultTheme) {
           line-height: 1.16em;
         }
       `,
-    )}
+  )}
 
     /* FONT-SIZE */
      ${Object.entries(theme.font.size).map(
-      ([k, v]) => css`
+    ([k, v]) => css`
         .fs-${k} {
           font-size: ${v}rem;
         }
       `,
-    )}
+  )}
   `
 }
 
@@ -77,35 +99,35 @@ function effectClasses(theme: DefaultTheme) {
   return css`
     /* GLOW */
     ${Object.keys(theme.effect.glow).flatMap((glowType) =>
-      Object.keys(theme.effect.glow[glowType as keyof ThemeEffect['glow']]).map(
-        (glowColor) => css`
+    Object.keys(theme.effect.glow[glowType as keyof ThemeEffect['glow']]).map(
+      (glowColor) => css`
           .fx-glow-${glowType}-${glowColor} {
             ${getThemeGlowEffectCss(
-              glowType as keyof ThemeEffect['glow'],
-              glowColor,
-            )}
+        glowType as keyof ThemeEffect['glow'],
+        glowColor,
+      )}
           }
         `,
-      ),
-    )}
+    ),
+  )}
 
     /* GLASS */
     ${Object.keys(theme.effect.glass).map(
-      (glassColor) => css`
+    (glassColor) => css`
         .fx-glass-${glassColor} {
           ${getThemeGlassEffectCss(glassColor)}
         }
       `,
-    )}
+  )}
 
     /* GLASS */
     ${Object.keys(theme.effect.dark).map(
-      (darkColor) => css`
+    (darkColor) => css`
         .fx-dark-${darkColor} {
           ${getThemeDarkEffectCss(darkColor)}
         }
       `,
-    )}
+  )}
   `
 }
 
