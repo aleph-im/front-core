da5b1147fd900ae54b7af0f5f72de8df7a0af131
amalcaraz <amalcaraz89@gmail.com>
2023-02-22 19:26:07 +0100

    added forwardRef to Button cmp

--- src/components/Button/cmp.stories.tsx
+++ src/components/Button/cmp.stories.tsx
@@ -15,7 +15,7 @@ export default {
   // },
 } as ComponentMeta<typeof Button>
 
-const defaultArgs: Partial<ButtonProps> = {
+const defaultArgs: Partial<ButtonProps<any>> = {
   as: 'button',
   children: 'Text',
   color: 'main2',
