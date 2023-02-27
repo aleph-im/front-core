31b44a82e1387c114e94d2d164b52d72ba92782e
Bonjour Internet <BjrInt@users.noreply.github.com>
2023-02-27 16:29:12 +0100

    feat: breadcrumb component

---
+++ src/components/Breadcrumb/cmp.stories.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { withDesign } from 'storybook-addon-designs'
+
+import Breadcrumb from './cmp'
+import { BreadcrumbProps } from './types'
+
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Components/UI/Breadcrumb',
+  component: Breadcrumb,
+  decorators: [withDesign],
+} as ComponentMeta<typeof Breadcrumb>
+
+const defaultArgs: Partial<BreadcrumbProps> = {}
+
+const defaultParams = {
+  controls: { exclude: ['color', 'size'] },
+}
+
+const navLinks = [
+  <a href="#">Home</a>,
+  <a href="#">Services</a>,
+  <a href="#">Cloud Storage</a>,
+]
+
+// ---
+
+const Template: ComponentStory<typeof Breadcrumb> = (args) => (
+    <Breadcrumb {...args} navLinks={navLinks} />
+)
+
+export const Default = Template.bind({})
+Default.args = {
+  ...defaultArgs,
+}
+Default.parameters = {
+  ...defaultParams,
+}
+
+// ---
