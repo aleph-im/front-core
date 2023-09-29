8e602a43bb164d80ea64af494401d3747e626f24
amalcaraz <amalcaraz89@gmail.com>
2023-09-29 12:28:26 +0200

    fix: label asterisk color red when there is an error

--- src/components/forms/FormLabel/cmp.tsx
+++ src/components/forms/FormLabel/cmp.tsx
@@ -5,7 +5,8 @@ import { FormLabelProps } from './types'
 export const FormLabel = ({ error, label, required }: FormLabelProps) => {
   return (
     <StyledContainer {...{ error, label }}>
-      {label} {required && <StyledRequiredSymbol>*</StyledRequiredSymbol>}
+      {label}{' '}
+      {required && <StyledRequiredSymbol error={error}>*</StyledRequiredSymbol>}
     </StyledContainer>
   )
 }
--- src/components/forms/FormLabel/styles.tsx
+++ src/components/forms/FormLabel/styles.tsx
@@ -3,6 +3,7 @@ import tw from 'twin.macro'
 import { addClasses } from '../../../utils'
 import { FormLabelProps } from './types'
 import { errorCss } from '../styles.forms'
+import { FormError } from '../FormError/types'
 
 export const StyledContainer = styled.span.attrs(
   addClasses('tp-body'),
@@ -11,8 +12,11 @@ export const StyledContainer = styled.span.attrs(
   ${errorCss}
 `
 
-export const StyledRequiredSymbol = styled.span.attrs(
-  addClasses('tp-body text-main0'),
-)`
+export const StyledRequiredSymbol = styled.span.attrs<{ error?: FormError }>(
+  ({ error, ...rest }) => {
+    const color = error ? '' : 'text-main0'
+    return addClasses(`tp-body ${color}`)(rest)
+  },
+)<{ error?: FormError }>`
   ${tw`align-top`}
 `
