e811b5acf5b4ade6ae57b471da5dbb1cbc89d3d3
amalcaraz <amalcaraz89@gmail.com>
2023-11-14 15:06:53 +0100

    fix: optional children in NotificationBadge

--- src/components/NotificationBadge/types.ts
+++ src/components/NotificationBadge/types.ts
@@ -3,6 +3,6 @@ import { HTMLAttributes } from 'react'
 export type NotificationBadgeVariant = 'success' | 'warning' | 'error' | 'info'
 
 export type NotificationBadgeProps = HTMLAttributes<HTMLSpanElement> & {
-  children: React.ReactNode
+  children?: React.ReactNode
   variant?: NotificationBadgeVariant
 }
