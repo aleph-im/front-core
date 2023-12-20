1db11af6ecede0e7267e0f04b8a8f81ca6efe2cf
amalcaraz <amalcaraz89@gmail.com>
2023-12-20 19:14:49 +0100

    fix: performance issue on useRequest triggerDeps

--- src/hooks/useRequest.ts
+++ src/hooks/useRequest.ts
@@ -39,7 +39,7 @@ export function useRequest<T>({
     if (!triggerOnMount) return
     request()
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, triggerDeps)
+  }, [...triggerDeps])
 
   return { ...reqState, request }
 }
