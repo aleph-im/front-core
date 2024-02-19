bd9af20373d9da2b8821397c30e2e720d4815525
amalcaraz <amalcaraz89@gmail.com>
2024-02-19 15:06:26 +0100

    fix: Tag variant prop optional

--- src/components/common/Tag/types.ts
+++ src/components/common/Tag/types.ts
@@ -1,6 +1,6 @@
 import { HTMLAttributes, ReactNode } from 'react'
 
 export type TagProps = HTMLAttributes<HTMLSpanElement> & {
-  variant: 'default' | 'accent'
+  variant?: 'default' | 'accent'
   children: ReactNode
 }
