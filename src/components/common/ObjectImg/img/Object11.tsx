f36c7cd5b91491b7c389946133fb187f41d0dad2
amalcaraz <amalcaraz89@gmail.com>
2024-06-11 14:38:44 +0200

    fix: ObjectImg size fix

--- src/components/common/ObjectImg/img/Object11.tsx
+++ src/components/common/ObjectImg/img/Object11.tsx
@@ -4,8 +4,8 @@ import { StyledObjectImgSvgProps } from '../types'
 export const Object11 = ({ color = '#ffffff' }: StyledObjectImgSvgProps) => {
   return (
     <svg
-      width="100"
-      height="100"
+      width="100%"
+      height="100%"
       viewBox="0 0 100 100"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
--- src/components/common/ObjectImg/img/Object18.tsx
+++ src/components/common/ObjectImg/img/Object18.tsx
@@ -7,8 +7,8 @@ export const Object18 = ({
 }: StyledObjectImgSvgProps) => {
   return (
     <svg
-      width="100"
-      height="100"
+      width="100%"
+      height="100%"
       viewBox="0 0 100 100"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
