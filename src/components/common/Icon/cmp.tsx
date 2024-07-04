acf5245fd92a441d26e8b7fbf6a5e90ed7801021
gmolki <gerardmo98@gmail.com>
2024-07-04 14:49:35 +0200

    feat: Add gradient to Icon

--- src/components/common/Icon/cmp.tsx
+++ src/components/common/Icon/cmp.tsx
@@ -14,21 +14,52 @@ import { StyledCustomIconCss, StyledIcon } from './styles'
 
 import * as customIcons from './custom/index'
 import styled, { useTheme } from 'styled-components'
+import { Gradient } from '../../../themes/types'
 
 // @todo: Think about it as we are including all the icons on the final bundle
 library.add(far, fab, fass)
 
 const iconPrefixes: IconPrefix[] = ['fass', 'fab', 'far', 'custom']
 
+const GradientDefinition = ({
+  gradient,
+  id,
+}: {
+  gradient?: Gradient
+  id?: string
+}) => {
+  if (!gradient) return null
+
+  return (
+    <svg width="0" height="0" tw="absolute">
+      <defs>
+        <linearGradient id={id}>
+          {gradient.colors.map((color, index) => (
+            <stop
+              key={index}
+              offset={gradient.stops[index] + '%'}
+              stopColor={color}
+            />
+          ))}
+        </linearGradient>
+      </defs>
+    </svg>
+  )
+}
+GradientDefinition.displayName = 'GradientDefinition'
+
 export const Icon = ({
   name,
   prefix = 'fass',
   size: $iconSize = 'md',
   color,
+  gradient,
   ...rest
 }: IconProps) => {
   const theme = useTheme()
   const $color = color ? theme.color[color] || color : color
+  const $gradient = gradient ? theme.gradient[gradient] : undefined
+  const $gradientId = $gradient ? `svg-gradient-${gradient}` : undefined
 
   for (const p of [prefix, ...iconPrefixes.filter((p) => p !== prefix)]) {
     if (p === 'custom') {
@@ -40,13 +71,17 @@ export const Icon = ({
       `
 
       return (
-        <StyledCustomIcon
-          {...{
-            $iconSize,
-            $color,
-            ...rest,
-          }}
-        />
+        <>
+          <GradientDefinitionMemo gradient={$gradient} id={$gradientId} />
+          <StyledCustomIcon
+            {...{
+              $iconSize,
+              $color,
+              $gradientId: $gradientId,
+              ...rest,
+            }}
+          />
+        </>
       )
     } else {
       const iconName = name as FAIconName
@@ -58,15 +93,19 @@ export const Icon = ({
       if (!icon) continue
 
       return (
-        <StyledIcon
-          {...{
-            icon,
-            size: '1x',
-            $iconSize,
-            color: $color,
-            ...rest,
-          }}
-        />
+        <>
+          <GradientDefinitionMemo gradient={$gradient} id={$gradientId} />
+          <StyledIcon
+            {...{
+              icon,
+              size: '1x',
+              $iconSize,
+              color: $color,
+              $gradientId: $gradientId,
+              ...rest,
+            }}
+          />
+        </>
       )
     }
   }
@@ -75,4 +114,7 @@ export const Icon = ({
 }
 Icon.displayName = 'Icon'
 
+export const GradientDefinitionMemo = memo(
+  GradientDefinition,
+) as typeof GradientDefinition
 export default memo(Icon) as typeof Icon
