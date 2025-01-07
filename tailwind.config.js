8b0d3520e5418c5ae54ce4e4635a1d9d1bcc7ce3
gmolki <gerardmo98@gmail.com>
2025-01-07 15:29:13 +0100

    fix: tailwind config ES module exporting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,25 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    screens: {
-      xs: '0rem', // 0px
-      sm: '36rem', // 576px
-      md: '48rem', // 768px
-      lg: '62rem', // 992px
-      xl: '75rem', // 1200px
-      '2xl': '87.5rem', // 1400px
-    },
-    fontSize: {
-      28: '1.75rem',
-      24: '1.5rem',
-      18: '1.125rem',
-      16: '1rem',
-      14: '0.875rem',
-      12: '0.75rem',
-      10: '0.625rem',
-    },
-    extend: {},
+
+export const content = ['./src/**/*.{js,jsx,ts,tsx}']
+export const theme = {
+  screens: {
+    xs: '0rem', // 0px
+    sm: '36rem', // 576px
+    md: '48rem', // 768px
+    lg: '62rem', // 992px
+    xl: '75rem', // 1200px
+    '2xl': '87.5rem', // 1400px
   },
-  plugins: [],
+  fontSize: {
+    28: '1.75rem',
+    24: '1.5rem',
+    18: '1.125rem',
+    16: '1rem',
+    14: '0.875rem',
+    12: '0.75rem',
+    10: '0.625rem',
+  },
+  extend: {},
 }
+export const plugins = []
